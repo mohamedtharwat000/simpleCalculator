898108fc75ba2122e2baecec2883a6db43fb49f8
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2023-03-19 20:10:52 +0200

    fix screen overflow

--- src/Components/Calculator/Screen.jsx
+++ src/Components/Calculator/Screen.jsx
@@ -4,10 +4,16 @@ export default function Screen(props) {
   const {
     calculatorData: { opration, result },
   } = props;
+  const oprationLength = opration
+    .split("")
+    .reverse()
+    .slice(0, 14)
+    .reverse()
+    .join("");
   return (
     <div>
-      <div className="screen py-4 mx-auto row row-cols-1 align-items-center text-end fs-1 border border-secondary">
-        <output name="opration">{opration}</output>
+      <div className="screen p-4 mx-auto row row-cols-1 align-items-center text-end fs-1 border border-secondary">
+        <output name="opration">{oprationLength}</output>
         <output name="result">{result}</output>
       </div>
     </div>
